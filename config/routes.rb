b087ecf5eb804a7446b430da20daa84d65d9ceb5
Haseeb Chaudhary <haseebchaudhary@gmail.com>
2018-06-12 12:08:42 +0200

    routes controller all sweet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :flats do
+    resources :bookings, only: [:new, :create]
+  end
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
