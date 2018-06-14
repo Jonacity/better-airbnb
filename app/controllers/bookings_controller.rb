156ceac779e8e6449d5066499b1f28dc7d1d36f8
jonathan sitti <sitti.jonathan@gmail.com>
2018-06-14 15:17:19 +0200

    bookings status buttons done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -27,10 +27,24 @@ class BookingsController < ApplicationController
     @user = current_user
 
     if @booking.update(booking_params)
-      redirect_to edit_user_path(@user)
+      redirect_to dashboard_bookings_path
     else
       render :edit
     end
+  end
+
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.accept
+
+    redirect_to dashboard_bookings_path
+  end
+
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.decline
+
+    redirect_to dashboard_bookings_path
   end
 
   private
